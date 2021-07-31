a4ffbbff77b0378440beef66ebcdf7c0160538b3
mar1usz <62397363+mar1usz@users.noreply.github.com>
2021-07-31 11:34:49 +0200

    move AddLinks above AddLink in Resource.cs

--- src/YellowFoods/Resources.Abstractions/Resource.cs
+++ src/YellowFoods/Resources.Abstractions/Resource.cs
@@ -6,8 +6,6 @@ namespace YellowFoods.Resources.Abstractions
     public abstract class Resource
     {
         public IList<Link> Links { get; set; } = new List<Link> { };
-
-        public void AddLink(Link link) => Links.Add(link);
         
         public void AddLinks(IEnumerable<Link> links)
         {
@@ -16,5 +14,7 @@ namespace YellowFoods.Resources.Abstractions
                 AddLink(link);
             }
         }
+
+        public void AddLink(Link link) => Links.Add(link);
     }
 }
